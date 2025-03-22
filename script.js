6780f0bf5eb5d8496703f3902042542ae97c3a6d
chynnalew <chynnalew@yahoo.com>
2025-03-21 20:02:52 -0700

    add canvas to html and function to set up and scale the canvas to different screen sizes and zooms

---
+++ script.js
@@ -0,0 +1,19 @@
+//Function to keep lines sharp when scaled up (larger screens/ zoomed in)
+function setupCanvas(canvas) {
+    // Get the device pixel ratio, falling back to 1.
+    var dpr = window.devicePixelRatio || 1;
+    // Get the size of the canvas in CSS pixels.
+    var rect = canvas.getBoundingClientRect();
+    // Give the canvas pixel dimensions of their CSS
+    // size * the device pixel ratio.
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+    var ctx = canvas.getContext('2d');
+    // Scale all drawing operations by the dpr, so you
+    // don't have to worry about the difference.
+    ctx.scale(dpr, dpr);
+    return ctx;
+}
+
+var canvas1 = document.getElementById('canvas');
+var ctx = setupCanvas(canvas1);
